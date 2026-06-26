2729521238e370efc4e6514cb90964a117be6135
YoussefKhalidAli <yossofwd@gmail.com>
2026-06-26 07:41:07 +0000

    chore: Change file names.

--- controller/kubernetes/connect.go
+++ controller/kubernetes/connect.go
@@ -1,45 +1,13 @@
 package kubernetes
 
-import (
-	"flag"
-	"os"
-	"path/filepath"
-	"time"
+import "fmt"
 
-	"k8s.io/client-go/informers"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/clientcmd"
+func AddService(obj any) {
+	fmt.Println("add", obj)
+	println("Added-----------------------------------------------------------")
+}
 
-	"kuberun.com/controller/utils"
-)
-
-func connect() {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		home, _ := os.UserHomeDir()
-		var kubeconfig *string = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-		flag.Parse()
-
-		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
-		if err != nil {
-			utils.HandelError(err, "KRC9020", "Controller couldn't create cluster config.")
-		}
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		utils.HandelError(err, "KRC9021", "Controller couldn't establish clientset")
-	}
-
-	factory := informers.NewSharedInformerFactory(clientset, time.Duration(utils.SyncTime)*time.Minute-time.Minute)
-
-	serviceInformer := factory.Core().V1().Services().Informer()
-
-	serviceInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{AddFunc: AddService, DeleteFunc: DeleteService})
-	stopCh := make(chan struct{})
-	factory.Start(stopCh)
-	println("we a go")
-	<-stopCh
+func DeleteService(obj any) {
+	fmt.Println("del", obj)
+	println("Deleted-----------------------------------------------------------")
 }
--- controller/kubernetes/informer.go
+++ controller/kubernetes/informer.go
@@ -1,13 +1,45 @@
 package kubernetes
 
-import "fmt"
+import (
+	"flag"
+	"os"
+	"path/filepath"
+	"time"
 
-func AddService(obj any) {
-	fmt.Println("add", obj)
-	println("Added-----------------------------------------------------------")
-}
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
 
-func DeleteService(obj any) {
-	fmt.Println("del", obj)
-	println("Deleted-----------------------------------------------------------")
+	"kuberun.com/controller/utils"
+)
+
+func connect() {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		home, _ := os.UserHomeDir()
+		var kubeconfig *string = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		flag.Parse()
+
+		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			utils.HandelError(err, "KRC9020", "Controller couldn't create cluster config.")
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		utils.HandelError(err, "KRC9021", "Controller couldn't establish clientset")
+	}
+
+	factory := informers.NewSharedInformerFactory(clientset, time.Duration(utils.SyncTime)*time.Minute-time.Minute)
+
+	serviceInformer := factory.Core().V1().Services().Informer()
+
+	serviceInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{AddFunc: AddService, DeleteFunc: DeleteService})
+	stopCh := make(chan struct{})
+	factory.Start(stopCh)
+	println("we a go")
+	<-stopCh
 }
