afc9ec4215c3070d0814517f36a70028fba5e8ad
YoussefKhalidAli <yossofwd@gmail.com>
2026-06-30 19:29:30 +0000

    chore: Move alert to new server package.

--- agent/server/alert.go
+++ agent/server/alert.go
@@ -1,13 +1,15 @@
-package main
+package server
 
 import (
 	"fmt"
 	"net/http"
 	"strings"
+
+	"kuberun.com/agent/store"
 )
 
 func Alert(ip string) {
-	controllerUrl := fmt.Sprintf("http://%v/alert", Config.KubeRunController)
+	controllerUrl := fmt.Sprintf("http://%v/alert", store.Config.KubeRunController)
 	println(controllerUrl)
 	http.Post(controllerUrl, "text/plain", strings.NewReader(ip))
 }
