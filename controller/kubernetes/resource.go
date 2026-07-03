d98a06adaeea244d0f494cbf8415c3d2ac2d44db
YoussefKhalidAli <yossofwd@gmail.com>
2026-07-03 13:39:10 +0000

    chore: Create seperate deployments file.

--- controller/kubernetes/resource.go
+++ controller/kubernetes/resource.go
@@ -2,17 +2,12 @@ package kubernetes
 
 import (
 	"context"
-	"fmt"
 	"time"
 
-	v1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/util/retry"
-	"kuberun.com/controller/store"
-	"kuberun.com/controller/utils"
 )
 
 func FindResource(clientset *kubernetes.Clientset, selectorMap map[string]string, resourceNamespace string, clusterIP string) (string, string) {
@@ -68,39 +63,4 @@ func FindResource(clientset *kubernetes.Clientset, selectorMap map[string]string
 	})
 
 	return resourceName, resourceKind
-}
-
-func LabelDeplyment(ctx context.Context, resourceNamespace string, deployment *v1.Deployment, clusterIP string) {
-	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		latest, err := clientset.AppsV1().Deployments(resourceNamespace).Get(ctx, deployment.Name, metav1.GetOptions{})
-		if err != nil {
-			return err
-		}
-
-		if latest.Labels == nil {
-			latest.Labels = make(map[string]string)
-		}
-		latest.Labels["kuberun/clusterIP"] = clusterIP
-		latest.Labels["kuberun/run"] = "true"
-
-		_, updateErr := clientset.AppsV1().Deployments(resourceNamespace).Update(
-			ctx,
-			latest,
-			metav1.UpdateOptions{},
-		)
-		return updateErr
-	})
-	if err != nil {
-		utils.HandelError(err, "KRC1443", fmt.Sprintf("Couldn't update deployment %v after retrying", deployment.Name))
-	}
-}
-
-func DeleteResource(clusterIP string) {
-	target := store.Targets[clusterIP]
-	target.Mux.Lock()
-	target.Resource = ""
-	target.ResourceName = ""
-	target.Status = "Awake"
-	target.Mux.Unlock()
-	println("deleted resource")
 }
