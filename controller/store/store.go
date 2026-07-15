76f1e734b1e87da2e3b99cd7ef80b82827d67cb9
YoussefKhaildAli <yossofwd@gmail.com>
2026-07-15 03:11:47 +0300

    chore: Move targets from store file.

--- controller/store/store.go
+++ controller/store/store.go
@@ -1,30 +1,11 @@
 package store
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
-	"sync"
 	"time"
-
-	"kuberun.com/controller/server"
 )
-
-type TargetDto struct {
-	LastAccessed time.Time
-	ResourceName string
-	Namespace    string
-	Resource     string
-	ServiceName  string
-	Status       string
-	UpdateMarker string
-	Mux          sync.Mutex       `json:"-"`
-	Servers      []*server.Switch `json:"-"`
-	Endpoints    []string
-	ServicePorts *[]int
-	SelectorMap  map[string]string
-}
 
 type AgentConfig struct {
 	KubeRunController string            `yaml:"kube_run_controller"`
@@ -32,8 +13,6 @@ type AgentConfig struct {
 	Ips               []string          `yaml:"ips"`
 	HeadlessMap       map[string]string `yaml:"headless_map"`
 }
-
-var Targets map[string]*TargetDto
 
 // Configs
 var syncMinutes time.Duration = getSyncMinutes()
@@ -44,26 +23,6 @@ var KubeRunAgent = getKubeRunAgent()
 
 // Labels
 var RunLabel = "kuberun/run=true"
-
-func PrintTargets() {
-	jsonData, err := json.MarshalIndent(Targets, "", "  ")
-	if err != nil {
-		fmt.Printf("Error printing map: %v\n", err)
-		return
-	}
-	fmt.Println(string(jsonData))
-}
-
-func (t *TargetDto) MarshalJSON() ([]byte, error) {
-	type Alias TargetDto
-	return json.Marshal(&struct {
-		*Alias
-		Mux    any `json:"Mux,omitempty"`    // Overwrite and ignore
-		Server any `json:"Server,omitempty"` // Overwrite and ignore
-	}{
-		Alias: (*Alias)(t),
-	})
-}
 
 func getSyncMinutes() time.Duration {
 	const defaultMinutes = 15
---
+++ controller/store/target.go
@@ -0,0 +1,47 @@
+package store
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	"kuberun.com/controller/server"
+)
+
+type TargetDto struct {
+	LastAccessed time.Time
+	ResourceName string
+	Namespace    string
+	Resource     string
+	ServiceName  string
+	Status       string
+	UpdateMarker string
+	Mux          sync.Mutex       `json:"-"`
+	Servers      []*server.Switch `json:"-"`
+	Endpoints    []string
+	ServicePorts *[]int
+	SelectorMap  map[string]string
+}
+
+var Targets map[string]*TargetDto
+
+func (t *TargetDto) MarshalJSON() ([]byte, error) {
+	type Alias TargetDto
+	return json.Marshal(&struct {
+		*Alias
+		Mux    any `json:"Mux,omitempty"`    // Overwrite and ignore
+		Server any `json:"Server,omitempty"` // Overwrite and ignore
+	}{
+		Alias: (*Alias)(t),
+	})
+}
+
+func PrintTargets() {
+	jsonData, err := json.MarshalIndent(Targets, "", "  ")
+	if err != nil {
+		fmt.Printf("Error printing map: %v\n", err)
+		return
+	}
+	fmt.Println(string(jsonData))
+}
